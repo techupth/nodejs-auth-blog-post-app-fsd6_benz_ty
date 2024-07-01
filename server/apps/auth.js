c61dd3bf64e8923fa95ace59e59fcd334f64b645
Wanchana <s.wanchana41@gmail.com>
2024-07-01 14:34:11 +0700

    feat: create Login API, exercise.2

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -4,6 +4,7 @@ const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+
 authRouter.post("/register", async (req, res) => {
   const newUser = {
     username: req.body.username,
@@ -24,5 +25,35 @@ authRouter.post("/register", async (req, res) => {
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
+
+authRouter.post("/login", async (req, res) => {
+  const user = await db.collection("users").findOne({
+    username: req.body.username,
+  });
+
+  const isvalidPassword = await bcrypt.compare(
+    req.body.password,
+    user.password
+  );
+
+  if (!user || !isvalidPassword) {
+    return res.status(404).json({
+      message: "Invalid username or password",
+    });
+  }
+
+  const token = jwt.sign(
+    { id: user._id, firstname: user.firstName, last: user.lastName },
+    process.env.SECRET_KEY,
+    {
+      expiresIn: "900000",
+    }
+  );
+
+  return res.json({
+    message: "login successfully",
+    token,
+  });
+});
 
 export default authRouter;
