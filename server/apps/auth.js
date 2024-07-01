e1240eccfeb280aa981c998162408b152771f73f
Wanchana <s.wanchana41@gmail.com>
2024-07-01 14:28:39 +0700

    feat: create Register API, exercise.1

--- server/apps/auth.js
+++ server/apps/auth.js
@@ -4,6 +4,23 @@ const authRouter = Router();
 
 // 🐨 Todo: Exercise #1
 // ให้สร้าง API เพื่อเอาไว้ Register ตัว User แล้วเก็บข้อมูลไว้ใน Database ตามตารางที่ออกแบบไว้
+authRouter.post("/register", async (req, res) => {
+  const newUser = {
+    username: req.body.username,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+  };
+  const salt = await bcrypt.genSalt(10);
+  newUser.password = await bcrypt.hash(newUser.password, salt);
+
+  const collection = db.collection("users");
+  await collection.insertOne(newUser);
+
+  return res.json({
+    message: "User has been created successfully",
+  });
+});
 
 // 🐨 Todo: Exercise #3
 // ให้สร้าง API เพื่อเอาไว้ Login ตัว User ตามตารางที่ออกแบบไว้
